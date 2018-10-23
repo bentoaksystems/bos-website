1b8cdc8b2786fc5c59dffbc61024ed009f7ebb1d
aminazar <amin@azarbadegan.net>
2018-10-23 04:52:15 +0330

    loose pricing schema

--- mongo/schema/pricing.schema.js
+++ mongo/schema/pricing.schema.js
@@ -1,27 +1,9 @@
 const Schema = require('mongoose').Schema;
 
 const schema_obj = {
-  title: {
-    type: Schema.Types.String,
-  },
-  planing_price: {
-    type: Schema.Types.String,
-  },
-  programming_price: {
-    type: Schema.Types.String
-  },
-  backing_price: {
-    type: Schema.Types.String
-  },
-  description1: {
-    type: Schema.Types.String,
-  },
-  description2: {
-    type: Schema.Types.String,
-  }
 };
 
 
-let pricingSchema = new Schema(schema_obj, {collection: 'pricing', strict: true});
+let pricingSchema = new Schema(schema_obj, {collection: 'pricing', strict: false});
 
 module.exports = pricingSchema;
